4f483014fdd149ebd1f0d29e84ef858b0c753992
Ward <wardv@tirf.ca>
2022-02-02 10:52:03 -0500

    created server

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require("./data/notes");
 
 function filterByQuery(query, notesArray) {
-    let filteredResults = notesArray;
-    if (query.title) {
-      filteredResults = filteredResults.filter(note => note.title === query.title);
-    }
-    return filteredResults;
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(
+      (note) => note.title === query.title
+    );
+  }
+  return filteredResults;
+}
+
+// "text" to be replaced with ID once ID is created!
+function findById(text, notesArray) {
+    const result = notesArray.filter(note => note.text === text)[0];
+    return result;
   }
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
+app.get("/api/notes", (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
-app.listen(3001, () => {
+// "text" to be replaced with ID once ID is created!
+app.get("/api/notes/:text", (req, res) => {
+  const result = findById(req.params.text, notes);
+  res.json(result);
+});
+
+app.listen(PORT, () => {
   console.log("API server now on port 3001!");
 });
