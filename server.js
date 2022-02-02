34b9618f90f02d416def040bec6f647574730136
Ward <wardv@tirf.ca>
2022-02-02 13:44:15 -0500

    finished adding post route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const { notes } = require("./data/notes");
 
 function filterByQuery(query, notesArray) {
@@ -10,14 +14,39 @@ function filterByQuery(query, notesArray) {
       (note) => note.title === query.title
     );
   }
+  if (query.text) {
+    filteredResults = filteredResults.filter(
+      (note) => note.text === query.text
+    );
+  }
   return filteredResults;
 }
 
-// "text" to be replaced with ID once ID is created!
-function findById(text, notesArray) {
-    const result = notesArray.filter(note => note.text === text)[0];
-    return result;
+function findById(id, notesArray) {
+  const result = notesArray.filter((note) => note.id === id)[0];
+  return result;
+}
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/notes.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
   }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+  return true;
+}
 
 app.get("/api/notes", (req, res) => {
   let results = notes;
@@ -27,10 +56,20 @@ app.get("/api/notes", (req, res) => {
   res.json(results);
 });
 
-// "text" to be replaced with ID once ID is created!
-app.get("/api/notes/:text", (req, res) => {
-  const result = findById(req.params.text, notes);
+app.get("/api/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
   res.json(result);
+});
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 app.listen(PORT, () => {
