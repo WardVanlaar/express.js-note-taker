bc60f41b53738d2931f6bcbb16e87c7daa1e1ff8
Ward <wardv@tirf.ca>
2022-02-02 14:39:14 -0500

    working on page submission functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { notes } = require("./data/notes");
 
 function filterByQuery(query, notesArray) {
@@ -71,6 +72,10 @@ app.post("/api/notes", (req, res) => {
     res.json(note);
   }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
   console.log("API server now on port 3001!");
